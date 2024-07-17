450c145d5f537796aebd41ee01ba9a688eca01a2
Eg0R98 <stulikove98@gmail.com>
2024-07-17 16:20:25 +0400

    Add files via upload

---
+++ Numbers.java
@@ -0,0 +1,42 @@
+package NumbersProperty;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Numbers {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Ввведите число: ");
+            int num = scanner.nextInt();
+            URL url = new URL("http://numbersapi.com/" + num);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+            con.setRequestMethod("GET");
+            con.setRequestProperty("Content-Type", "application/json");
+            con.setConnectTimeout(2000);
+            con.setReadTimeout(2000);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println(inputLine);
+            }
+
+
+        } catch (InputMismatchException e) {
+            System.out.println("Ошибка. Некорректный ввод.");
+        } catch (MalformedURLException | ProtocolException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
